c963769ec9c898a46a81878ffda403ef70a3b18f
coder123776 <ddagmax@gmail.com>
2025-07-12 21:06:56 +0200

    updated trait

---
+++ src/Traits/ModelAccessors.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace LaravelShared\Core\Traits;
+
+use Illuminate\Support\Str;
+
+trait ModelAccessors
+{
+    public function __call($method, $parameters)
+    {
+        if (Str::startsWith($method, 'get')) {
+            $attribute = Str::snake(Str::after($method, 'get'));
+
+            // Bestaat het veld in $fillable of $attributes?
+            if (
+                (property_exists($this, 'fillable') && in_array($attribute, $this->fillable)) ||
+                array_key_exists($attribute, $this->attributes)
+            ) {
+                return $this->attributes[$attribute] ?? null;
+            }
+        }
+
+        return parent::__call($method, $parameters);
+    }
+}
