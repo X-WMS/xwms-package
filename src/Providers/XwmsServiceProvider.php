1d325c3cd9719f7434c334f5f41a70e890f874d7
coder123776 <ddagmax@gmail.com>
2025-08-08 20:14:36 +0200

    updated package boot

--- src/Providers/XwmsServiceProvider.php
+++ src/Providers/XwmsServiceProvider.php
@@ -16,33 +16,6 @@ class XwmsServiceProvider extends ServiceProvider
     {
         $this->publishes([
             __DIR__.'/../config/xwms.php' => config_path('xwms.php'),
-        ], 'config');
-
-        $this->syncConfigKeys();
-    }
-
-    protected function syncConfigKeys()
-    {
-        $defaultConfig = require __DIR__.'/../config/xwms.php';
-        $userConfigPath = config_path('xwms.php');
-
-        if (!file_exists($userConfigPath)) {
-            return; // niks te doen als user config niet bestaat
-        }
-
-        $userConfig = require $userConfigPath;
-
-        $mergedConfig = array_replace_recursive($defaultConfig, $userConfig);
-
-        // Voeg keys uit defaultConfig toe die ontbreken in userConfig
-        foreach ($defaultConfig as $key => $value) {
-            if (!array_key_exists($key, $userConfig)) {
-                $userConfig[$key] = $value;
-            }
-        }
-
-        // Schrijf terug naar config/xwms.php
-        $configContent = "<?php\n\nreturn " . var_export($userConfig, true) . ";\n";
-        file_put_contents($userConfigPath, $configContent);
+        ], 'xwms-config');
     }
 }
